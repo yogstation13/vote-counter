1d07802e71010841bd448b2d9df069253ce7d242
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2023-08-27 16:12:02 -0400

    Works on mobile now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, HostListener, OnInit } from "@angular/core";
 import { DrandService } from "./drand.service";
 import { VoteService } from "./vote.service";
 
@@ -7,7 +7,14 @@ import { VoteService } from "./vote.service";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public innerWidth = 0;
+
+  @HostListener("window:resize")
+  ngOnInit() {
+    this.innerWidth = window.innerWidth;
+  }
+
   constructor(
     public voteService: VoteService,
     public drandService: DrandService,
